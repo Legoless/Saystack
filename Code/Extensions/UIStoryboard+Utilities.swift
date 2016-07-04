550e5912bf873b159e86e094b3fc76c5a3c11394
Dal Rupnik <legoless@gmail.com>
2016-07-04 15:34:34 +0200

    Added storyboard utilities

---
+++ Code/Extensions/UIStoryboard+Utilities.swift
@@ -0,0 +1,16 @@
+//
+//  UIStoryboard+Utilities.swift
+//  Saystack
+//
+//  Created by Dal Rupnik on 04/07/16.
+//  Copyright © 2016 Unified Sense. All rights reserved.
+//
+
+import UIKit
+
+public extension UIStoryboard {
+    public class func initialViewControllerInStoryboard(name: String, bundle: NSBundle? = nil) -> UIViewController? {
+        let storyboard = UIStoryboard(name: name, bundle: bundle)
+        return storyboard.instantiateInitialViewController()
+    }
+}
