2e177034095cc7a79eec10143d2cd5c5b510f6cd
Dal Rupnik <legoless@gmail.com>
2016-10-17 15:18:28 +0200

    Added navigation controller category

---
+++ Code/Extensions/UIKit/UINavigationController+Utilities.swift
@@ -0,0 +1,18 @@
+//
+//  UINavigationController+Utilities.swift
+//  Saystack
+//
+//  Created by Dal Rupnik on 17/10/16.
+//  Copyright © 2016 Unified Sense. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    public func pushViewControllerStack(_ viewControllers: [UIViewController], animated: Bool) {
+        var currentViewControllers = self.viewControllers
+        currentViewControllers.append(contentsOf: viewControllers)
+        
+        setViewControllers(currentViewControllers, animated: animated)
+    }
+}
