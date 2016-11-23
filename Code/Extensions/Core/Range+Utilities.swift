f4e576472f7518f4d2914c4c9b107f32528d436c
Dal Rupnik <legoless@gmail.com>
2016-11-23 13:34:57 +0100

    Added range extensions

---
+++ Code/Extensions/Core/Range+Utilities.swift
@@ -0,0 +1,27 @@
+//
+//  Range+Utilities.swift
+//  Saystack
+//
+//  Created by Dal Rupnik on 23/11/2016.
+//  Copyright © 2016 Unified Sense. All rights reserved.
+//
+
+import Foundation
+
+//
+// Original author Martin R. from http://stackoverflow.com/questions/25138339/nsrange-to-rangestring-index
+//
+
+extension String {
+    func nsRange(from range: Range<String.Index>) -> NSRange {
+        let from = range.lowerBound.samePosition(in: utf16)
+        let to = range.upperBound.samePosition(in: utf16)
+        return NSRange(location: utf16.distance(from: utf16.startIndex, to: from), length: utf16.distance(from: from, to: to))
+    }
+
+    func range(from nsRange: NSRange) -> Range<String.Index>? {
+        guard let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex), let to16 = utf16.index(from16, offsetBy: nsRange.length, limitedBy: utf16.endIndex), let from = String.Index(from16, within: self), let to = String.Index(to16, within: self) else { return nil }
+        
+        return from ..< to
+    }
+}
