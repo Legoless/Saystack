31872216f7a92f5569d88b95172ad5a90a034cfb
Dal Rupnik <legoless@gmail.com>
2016-11-23 12:36:18 +0100

    Carthage support

---
+++ Code/Demo/Saystack/Saystack.h
@@ -0,0 +1,19 @@
+//
+//  Saystack.h
+//  Saystack
+//
+//  Created by Dal Rupnik on 23/11/2016.
+//  Copyright © 2016 Unified Sense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Saystack.
+FOUNDATION_EXPORT double SaystackVersionNumber;
+
+//! Project version string for Saystack.
+FOUNDATION_EXPORT const unsigned char SaystackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Saystack/PublicHeader.h>
+
+
---
+++ Code/Demo/SaystackMac/SaystackMac.h
@@ -0,0 +1,19 @@
+//
+//  SaystackMac.h
+//  SaystackMac
+//
+//  Created by Dal Rupnik on 23/11/2016.
+//  Copyright © 2016 Unified Sense. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SaystackMac.
+FOUNDATION_EXPORT double SaystackMacVersionNumber;
+
+//! Project version string for SaystackMac.
+FOUNDATION_EXPORT const unsigned char SaystackMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SaystackMac/PublicHeader.h>
+
+
