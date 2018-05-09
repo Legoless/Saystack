3fdb57986e42c946a3754067337de631d200a6fc
Dal Rupnik <legoless@gmail.com>
2018-05-09 09:47:00 +0200

    Add encodable utilities

---
+++ Code/Extensions/Core/Encodable+Utilities.swift
@@ -0,0 +1,27 @@
+//
+//  Encodable+Utilities.swift
+//  Saystack
+//
+//  Created by Dal Rupnik on 5/9/18.
+//  Copyright © 2018 Unified Sense. All rights reserved.
+//
+
+import Foundation
+
+public extension Encodable {
+    public func jsonString(with encoder: JSONEncoder) -> String? {
+        do {
+            let encodedObject = try encoder.encode(self)
+            
+            if let json = String(data: encodedObject, encoding: .utf8) {
+                return json
+            }
+            
+            return nil
+        }
+        catch {
+            return nil
+        }
+    }
+}
+
