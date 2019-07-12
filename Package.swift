60cd9f39d57f473f5c72b7d1995acf8b2b638256
Dal Rupnik <legoless@gmail.com>
2019-07-12 11:29:11 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "BlubBlubCore",
+    name: "Saystack",
     products: [
         .library(name: "Saystack", targets: ["Saystack"]),
     ],
